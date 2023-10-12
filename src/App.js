f7e7f5e2b5ecce3083478d7e603d198eebfcd3f4
v.davidovich <dalvoid321@gmail.com>
2023-10-12 14:33:52 +0300

    Connect to backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql} from "@apollo/client";
+
+const GET_FRACTIONS = gql`
+  query Fractions {
+    fractions {
+        id
+        name
+    }
+  }
+`;
+
+function DisplayFractions() {
+  const { loading, error, data } = useQuery(GET_FRACTIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.fractions.map(({ id, name }) => (
+     <div key={id}>
+       <h3>{name}</h3>
+     </div>
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h2>Fractions</h2>
+        <br/>
+        <DisplayFractions/>
+      </div>
   );
 }
 
